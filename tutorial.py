d72ac6d4152cc923504c91a344b15a9ba891abf5
InTh3ory <jason.roett@gmail.com>
2013-04-19 20:25:09 -0700

    test

--- tutorial.py
+++ tutorial.py
@@ -1,19 +1,35 @@
 import web
 
-render = web.template.render('templates/')
 
 urls = (
-	'/(.*)','index'
+	'/(.*)','index',
+    '/upload', 'Upload'
 	)
 
+my_form = web.form.Form(web.form.Textbox('', class_='textfield', id='textfield'), )
+
 class index:
-	def GET(self,name):
-		#return "Hello World!"
-		#name = 'Neo'
-		#i = web.input(name=None)
-		return render.index(name)
+	def GET(self, name):
+		form = my_form()
+		return render.index(form, "Index")
+
+class Upload:
+    def GET(self):
+        form = my_form()
+        return render.upload(form, "Hacha")
+
+    def POST(self, test):
+        form = my_form()
+        form.validates()
+        s = form.value['textfield']
+        return make_text(s)
+
+
+
+render = web.template.render('templates/')
 
 if __name__ == "__main__":
 	app = web.application(urls, globals())
 	app.run()
-#COMMIT TEST
+
+
